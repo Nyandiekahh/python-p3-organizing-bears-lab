b1b526850b4d1e2f52b7692ad6a330c6c86b309b
Nyandieka <einsteinmokua100@gmail.com>
2024-07-19 08:10:44 +0300

    Passes all the tests

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 5, 'F', 'Black', 'Calm', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Aggressive', 0),
+('Melissa', 8, 'F', 'White', 'Calm', 1),
+('Grinch', 4, 'M', 'Brown', 'Aggressive', 1),
+('Wendy', 6, 'F', 'Black', 'Calm', 1),
+(NULL, 3, 'F', 'Brown', 'Calm', 1); -- unnamed bear
